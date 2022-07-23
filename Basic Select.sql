019b968e3d8f24623527c6087441bbd404b4c0b5
Minyoung Kim <93497667+min02yam@users.noreply.github.com>
2022-07-23 13:49:53 +0900

    0723

--- Basic Select.sql
+++ Basic Select.sql
@@ -8,3 +8,21 @@ ORDER BY CITY ;
 SELECT COUNT(CITY)- COUNT(DISTINCT CITY)
 FROM STATION 
 
+-- Weather Observation Station 5
+-- !!mySQL은 집합 연산자 사용 시 괄호 
+(SELECT CITY,  LENGTH(CITY) AS length
+FROM STATION
+ORDER BY length, CITY
+LIMIT 1)
+UNION
+(SELECT CITY,  LENGTH(CITY) AS length
+FROM STATION
+ORDER BY length DESC, CITY
+LIMIT 1);
+
+-- Weather Observation Station 6
+-- 1. a, e, i, o, or u로 시작하는 CITY NAME
+-- 2. 중복X
+SELECT 
+
+
